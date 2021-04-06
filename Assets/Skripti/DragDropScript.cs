2d3fda52882864ad492a2e83f756a7da1bc16f72
Richards Griezitis <ricis1818@gmail.com>
2021-04-06 11:43:26 +0300

    Uzsākta pirmā skripta rakstīšana

---
+++ Assets/Skripti/DragDropScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropScript : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler /*, IEndDragHandler*/ {
+    public Objekti ObjektuSkripts;
+    private CanvasGroup kanvasGrupa;
+    private RectTransform velkObjRectTrasf;
+
+
+    void Awake()
+    {
+
+        kanvasGrupa = GetComponent<CanvasGroup>();
+
+        velkObjRectTrasf = GetComponent<RectTransform>();
+
+    }
+
+ public void OnPointerDown(PointerEventData notikums)
+    {
+        Debug.Log("Uzklikšķināts uz velkamā objekta!");
+    }
+
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzskākta vilkšana!");
+
+        kanvasGrupa.alpha = 0.6f;
+
+        kanvasGrupa.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData notikums)
+    {
+        Debug.Log("Notike vilkšana!");
+        velkObjRectTrasf.anchoredPosition += notikums.delta / ObjektuSkripts.kanva.sclaeFactor;
+    }
+}
