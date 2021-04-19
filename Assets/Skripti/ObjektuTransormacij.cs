8d30109f2eb5e45f394395712ccd0dfd4d38e173
Richards Griezitis <ricis1818@gmail.com>
2021-04-19 14:31:44 +0300

    Pabeigta projekta pirmā daļa (kopīgā daļa)

---
+++ Assets/Skripti/ObjektuTransormacij.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjektuTransormacij : MonoBehaviour
+{
+
+    public Objekti objektuSkripts;
+
+
+    void Update()
+    {
+        if (objektuSkripts.pedejaisVilktais != null)
+        {
+
+
+            if (Input.GetKey(KeyCode.Z))
+            {
+                objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.Rotate(0, 0, Time.deltaTime * 9f);
+            }
+
+            if (Input.GetKey(KeyCode.X))
+            {
+                objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.Rotate(0, 0, -Time.deltaTime * 9f);
+            }
+
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x > 0.35)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale =
+                        new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x - 0.001f,
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.y);
+                }
+            }
+
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x < 0.9)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale =
+                        new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x + 0.001f,
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.y);
+                }
+            }
+
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x < 0.8)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale =
+                        new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x,
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.y + 0.001f);
+                }
+            }
+
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x > 0.35)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale =
+                        new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.x,
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale.y - 0.001f);
+                }
+            }
+
+        }
+    }
+}
