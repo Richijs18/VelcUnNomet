f53ed29d996ade3b139780489d57a641ba648a9f
Richards Griezitis <ricis1818@gmail.com>
2021-04-25 18:26:26 +0300

    Pievienota galvena izvelne

---
+++ Assets/Skripti/IzvelneScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IzvelneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void uzIzvelni()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+}
